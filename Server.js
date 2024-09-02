58721e23d4178c44b810322fbdbe595db298ce09
Yarragorla Soujanya <117831336+soujanya2308@users.noreply.github.com>
2024-09-02 19:34:36 +0530

    Add files via upload

---
+++ Server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require("cors");
+
+const routes = require("./routes/ToDoRoute");
+
+require('dotenv').config()
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(cors());
+app.use(express.json());
+
+mongoose 
+.connect(process.env.MONGODB_URL,
+    { useNewUrlParser:true,
+        useUnifiedTopology:true,
+    })
+.then(() => console.log("Connected to MongoDB..."))
+.catch((err) => console.log("MongoDB connection error:",err))
+
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Listening on:${PORT}`))
